c31085355557cf6b01d7e3ee3e06986eeecc9e95
Boniswah <boniswah@gmail.com>
2023-08-31 10:43:45 +0200

    changed api key
    the other one was giving axios error 401

--- src/Weather.js
+++ src/Weather.js
@@ -32,7 +32,7 @@ export default function Weather(props) {
   }
 
   function search() {
-    const apiKey = "046370a13a995d289a8dbcd534de0877";
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
